dc59a061d1cc56c21fc8890c343942db5aa0a482
slavik2011 <89517629+slavik2011@users.noreply.github.com>
2024-10-24 11:47:28 -0700

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,7 +1,8 @@
 #!/bin/env python
 from app import create_app, socketio
+import sys
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=int(sys.argv[1]))
